e87a21fd717953e50546dbd7f60a6f6ddaa34a79
Bryant <bryant.steed@gmail.com>
2026-02-05 09:44:53 -0700

    updated dir

--- data_cleaning/generate_sample_array_ids.sh
+++ data_cleaning/generate_sample_array_ids.sh
@@ -8,11 +8,11 @@
 # output file we want to create.
 # We also only need to run this once
 
-$fastq_dir="$(pwd)/fastq"
-$output_file="$(pwd)/sample_array_ids.txt"
+fastq_dir="/home/${USER}/groups/grp_tb/fastq"
+output_file="sample_array_ids.txt"
 
 i=1
-for r1filename in $(ls $fastq_dir/*_R1.fastq.gz); do
+for r1filename in "${fastq_dir}"/*_R1.fastq.gz; do
     r2filename="${r1filename/_R1.fastq.gz/_R2.fastq.gz}"
     echo "${i}"$'\t'"${r1filename}"$'\t'"${r2filename}" >> $output_file
     ((i++))
