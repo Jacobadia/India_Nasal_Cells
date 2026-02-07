35c46be8edb8a7d2fba51c7eae26de29470c78fd
Bryant <bryant.steed@gmail.com>
2026-02-07 15:19:43 -0700

    made quality_control.sh the main file. We'll be running everything synchronously on this one sbatch job

--- data_cleaning/quality_control.sh
+++ data_cleaning/quality_control.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+#SBATCH --job-name=quality_control
+#SBATCH --output=/grphome/grp_tb/processing_scripts/quality_control.out
+#SBATCH --error=/grphome/grp_tb/processing_scripts/quality_control.err
+#SBATCH --mail-user=bryant.steed@gmail.com
+#SBATCH --time=5:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=22
+#SBATCH --mem=150G
+
 # Run Trimmomatic with our parameters
-srun run_trimmomatic.sh
+trimmomatic.sh
 
 # run the FastQC quality control script on the trimmed sequences
-srun run_fastqc.sh
+# this should aggregate the summary.txt files too
+fastqc_trimmed.sh
 
-# This will aggregate all FastQC reports into a single MultiQC report
-# We will look at this report to determine if further cleaning is necessary
-srun run_multiqc.sh
+# run multiQC. If we decide to add this
