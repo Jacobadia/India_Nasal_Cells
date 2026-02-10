d44049b735b4c469a49d51f9ab62a92382ccc763
Jacob <Jacobemartinez2000@gmail.com>
2026-02-10 13:41:52 -0700

    Roc Models File

---
+++ R/roc_models_github.R
@@ -0,0 +1,18 @@
+# ROC and models
+
+####   Libraries   ####
+library(tidyverse)
+library(DESeq2)
+library(caret)
+library(ggplot2)
+
+library(doParallel)
+library(devEMF)
+library(MLeval)
+library(sva)
+library(patchwork)
+library(cowplot)
+library(ranger)
+library(glmnet)
+library(kernlab)
+library(pls)
