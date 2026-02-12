534cb549dd8600d640d873430c6f18e4cd60c610
Jacob <Jacobemartinez2000@gmail.com>
2026-02-12 14:50:13 -0700

    Initial Outline

--- R/roc_models_github.R
+++ R/roc_models_github.R
@@ -16,3 +16,27 @@ library(ranger)
 library(glmnet)
 library(kernlab)
 library(pls)
+
+### theme ###
+
+### modling data frame ###
+
+# Convert to matrix
+
+# DESeqq2, matrix -> DESeqq object for machine learing
+
+#Get and filter for significant genes
+
+### machine learnig models ###
+
+### Variable importance
+
+#Generate importance plots
+
+#Find overlaps
+
+### Refit nasal models based on overlaps
+
+### machine learnig models ###
+
+#### Figures  ####
