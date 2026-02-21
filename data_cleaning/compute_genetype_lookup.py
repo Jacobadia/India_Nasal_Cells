44c1edfa7c2cdaaafea47218cd3261a17668728d
Bryant <bryant.steed@gmail.com>
2026-02-20 21:05:00 -0700

    made a quick script to go through out annottations gtf file, parse it and make another file that maps gene ensemble ids to whether they are protein encoding or not. The idea would be to maybe only look at those so we can get more statistical power

---
+++ data_cleaning/compute_genetype_lookup.py
@@ -0,0 +1,46 @@
+#!usr/bin/env python3
+
+gtf_file = "/grphome/grp_tb/star_genome/gencode.v49.primary_assembly.annotation.gtf"
+output_file = "/grphome/grp_tb/processing_scripts/results/documents/gene_type_lookup.txt"
+
+def is_qualified_entry(line, fields):
+    if line.startswith("#"):
+        return False
+    if len(fields) < 9:
+        print("Warning: Line does not have 9 fields, skipping it")
+        return False
+    feature_type = fields[2].lower()
+    if feature_type != "gene":
+        return False
+    return True
+
+def process_line(line):
+    fields = line.strip().split("\t")
+    if not is_qualified_entry(line, fields):
+        return
+    attributes = fields[8]
+    attributes.split(";")
+    attribute_dict = {}
+    for attribute in attributes.split(";"):
+        key, value = attribute.strip().split(" ", 1)
+        attribute_dict[key] = value.strip('"')
+    gene_id = attribute_dict.get("gene_id", "NA")
+    gene_type = attribute_dict.get("gene_type", "NA")
+    if gene_id == "NA":
+        print("Warning: gene_id not found in attributes, skipping line")
+        return
+    if gene_type == "NA":
+        print(f"Warning: gene_type not found for gene_id {gene_id}, setting it to NA")
+    write_output(gene_id, gene_type)
+
+def write_output(gene_id, gene_type):
+    with open(output_file, "a") as f:
+        f.write(f"{gene_id}\t{gene_type}\n")
+
+def main():
+    with open(gtf_file) as f:
+        for line in f:
+            process_line(line)
+
+if __name__ == "__main__":
+    main()
