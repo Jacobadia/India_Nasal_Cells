57ba8e78b338b2c23dc3577cce574d9aec03d481
Bryant <bryant.steed@gmail.com>
2026-02-22 21:37:18 -0700

    added logic to also parse out genetype_lookup.txt file to filter out the sequences that are non protein coding

--- R/differential.R
+++ R/differential.R
@@ -24,11 +24,23 @@ conditionData <- data.frame(condition = condition)
 rownames(conditionData) <- sample_names
 
 # Filter out genes with no counts
-keep_genes <- rowSums(counts) > 0
-throw_away_genes <- rowSums(counts) <= 0
-print(paste("Number of genes with no counts:", sum(throw_away_genes)))
-print(paste("Number of genes with counts:", sum(keep_genes)))
-counts <- counts[keep_genes, ]
+non_zero_genes <- rowSums(counts) > 0
+zero_genes <- rowSums(counts) <= 0
+print(paste("Number of genes with no counts:", sum(zero_genes)))
+print(paste("Number of genes with counts:", sum(non_zero_genes)))
+counts <- counts[non_zero_genes, ]
+
+# Filter out genes that are not protein coding
+
+genetype_lookup <- read.delim("genetype_lookup.txt", header = FALSE, stringsAsFactors = FALSE)
+colnames(genetype_lookup) <- c("gene_id", "gene_type")
+
+# Keep only protein coding genes
+protein_coding_genes <- genetype_lookup$gene_id[genetype_lookup$gene_type == "protein_coding"]
+print(paste("Number of protein coding genes:", length(protein_coding_genes)))
+non_protein_coding_genes <- length(unique(rownames(counts))) - length(protein_coding_genes)
+print(paste("Number of non-protein coding genes:", non_protein_coding_genes))
+counts <- counts[rownames(counts) %in% protein_coding_genes, ]
 
 dds <- DESeqDataSetFromMatrix(countData = counts,
                               colData = conditionData,
