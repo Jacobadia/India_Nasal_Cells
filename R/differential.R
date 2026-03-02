0adf1b74062c41bc8578da9ed9c34b3a3b158ff8
Bryant <bryant.steed@gmail.com>
2026-03-02 15:59:05 -0700

    factored out genetype filtering and mean based filtering. Also, my anaylsis files are in an artifacts dir that I gitignore

--- R/differential.R
+++ R/differential.R
@@ -4,7 +4,7 @@
 #     install.packages("BiocManager")
 
 # BiocManager::install("DESeq2")
-library(DESeq2)
+suppressMessages(suppressWarnings(library(DESeq2)))
 source("./parse_counts.R")
 source("./filtration.R")
 
@@ -22,17 +22,9 @@ conditionData <- get_counts_and_condition_data_output$conditionData
 genetype_lookup <- get_genetype_lookup(gene_type_file)
 counts <- filter_total_counts(counts, 0)
 
-# Keep only protein coding genes
-protein_coding_genes <- genetype_lookup$gene_id[genetype_lookup$gene_type == "protein_coding"]
-print(paste("Number of protein coding genes:", length(protein_coding_genes)))
-non_protein_coding_genes <- length(unique(rownames(counts))) - length(protein_coding_genes)
-print(paste("Number of non-protein coding genes:", non_protein_coding_genes))
-counts <- counts[rownames(counts) %in% protein_coding_genes, ]
+counts <- filter_protein_coding_genes(counts, genetype_lookup)
 
-
-# keep only that have an average count of at least 10 across all samples
-average_counts <- rowMeans(counts)
-counts <- counts[average_counts >= 10, ]
+counts <- filter_mean_counts(counts, 10)
 
 
 dds <- DESeqDataSetFromMatrix(countData = counts,
--- R/filtration.R
+++ R/filtration.R
@@ -3,8 +3,29 @@ filter_total_counts <- function(counts, threshold = 0) {
   # to the threshold across all samples
   passing_genes <- rowSums(counts) > threshold
   failing_genes <- rowSums(counts) <= threshold
-  print(paste("Number of genes  with total counts <=", threshold, ":", sum(failing_genes)))
   print(paste("Number of genes with total counts >", threshold, ":", sum(passing_genes)))
+  print(paste("Number of genes  with total counts <=", threshold, ":", sum(failing_genes)))
   counts <- counts[passing_genes, ]
   return (counts)
+}
+
+filter_protein_coding_genes <- function(counts, genetype_lookup) {
+  # Keep only protein coding genes
+  protein_coding_genes <- genetype_lookup$gene_id[genetype_lookup$gene_type == "protein_coding"]
+  print(paste("Number of protein coding genes:", length(protein_coding_genes)))
+  non_protein_coding_genes <- length(unique(rownames(counts))) - length(protein_coding_genes)
+  print(paste("Number of non-protein coding genes:", non_protein_coding_genes))
+  counts <- counts[rownames(counts) %in% protein_coding_genes, ]
+  return (counts)
+}
+
+filter_mean_counts <- function(counts, threshold) {
+  # keep only that have an average count of at least n across all samples
+  average_counts <- rowMeans(counts)
+  passing_genes <- average_counts >= threshold
+  failing_genes <- average_counts < threshold
+  print(paste("Number of genes with average counts < ", threshold, ":", sum(failing_genes)))
+  print(paste("Number of genes with average counts >= ", threshold, ":", sum(passing_genes)))
+  counts <- counts[average_counts >= threshold, ]
+  return(counts)
 }
